4d4dd61e428fdd8cfabb30bd793388cb86c2bd40
SuspiciousScapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-06-29 17:00:49 +0200

    [REFACTOR] Now statements and expressions are parsed separately

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,11 +1,20 @@
-import { isAlpha, isNumeric, isAlphaNum, isSpace } from "../utils/chars.ts"
+/**
+ * Lexer/Scanner prototype for ScrapLang
+ * 
+ * Lexer scan a file and reads his contents to get tokens that correspond to the tokens defined in the language syntax
+ * For example, const, fn, enum... are keywords and Lexer identifies as such
+ * 
+ * If Lexer founds a keyword or another token that is invalid, will throw an error
+ */
 
-import LexingError from "./LexerError.ts"
+import { isAlpha, isNumeric, isAlphaNum, isSpace, isBinaryLiteral, isOctalLiteral, isHexaLiteral } from "../utils.ts"
+
+import LexingError from "./lexer-error.ts"
 import LexerCursor from "./lexer-cursor.ts"
 
 export type TokenType = |
   "IdentifierName"      |
-  "Punctuator"          |
+  "Statement"           |
   "NumericLiteral"      |
   "FloatLiteral"        |
   "BinaryLiteral"       |
@@ -45,7 +54,10 @@ export enum Tokens {
   SEMICOLON     = ';',
   EXCLAMATION   = '!',
   INTERROGATION = '?',
-  UNDERSCORE    = '_'
+  UNDERSCORE    = '_',
+
+  // Spacials Tokens (e.g: module accessor)
+  MODULE_ACCESSOR = "::"
 }
 
 export enum Keywords {
@@ -100,24 +112,38 @@ export enum Keywords {
 }
 
 export interface Token {
-  type: TokenType
+  type: TokenType,
   content: string,
+  pos: number,
+  line: number
 }
 
 export default class Lexer {
-  line: number
   cursor: LexerCursor
   fileName: string
+  line: number
 
   public constructor(source: string, fileName: string) {
-    this.line = 0
-    this.fileName = fileName
     this.cursor = new LexerCursor(source)
+    this.fileName = fileName
+    this.line = 1
   }
 
-  /* public lexNew(source: string) {
+  /**
+   * Sets the a lexer object ready to read a new file and tokenize his content
+   * @param otherFileName Name of the new file to be scanned
+   * @param otherSource 
+   */
+  public alsoScan(otherFileName: string, otherSource: string) {
+    // Configure the lexer
+    this.line = 1
+    this.fileName = otherFileName
 
-  } */
+    // configure the cursor
+    this.cursor.source = otherSource
+    this.cursor.currentTok = otherSource.at(0)!
+    this.cursor.pos = 0
+  }
 
   /**
    * Returns the next char, advancing the cursor 1 position
@@ -129,7 +155,7 @@ export default class Lexer {
 
   /**
    * Aux method to check the next token
-   * @param char 
+   * @param char Possible next char
    * @returns 
    */
   private checkNext(char: string): boolean {
@@ -146,8 +172,6 @@ export default class Lexer {
       case Keywords.FROM:
       case Keywords.EXPORT:
       case Keywords.CLASS:
-      case Keywords.THIS:
-      case Keywords.SUPER:
       case Keywords.TYPE:
       case Keywords.INTERFACE:
       case Keywords.ENUM:
@@ -172,8 +196,6 @@ export default class Lexer {
       case Keywords.TRY:
       case Keywords.CATCH:
       case Keywords.AS:
-      case Keywords.CONSTRUCTOR:
-      case Keywords.DESTRUCTOR:
       case Keywords.NEW:
       case Keywords.DROP:
       case Keywords.PUBLIC:
@@ -185,15 +207,15 @@ export default class Lexer {
       case Keywords.GETTER:
       case Keywords.ASYNC:
       case Keywords.AWAIT: {
-        return { type: "Keyword", content: identifier }
+        return { type: "Statement", content: identifier, line: this.line, pos: this.cursor.pos }
       }
 
       case "instanceof": {
-        return { type: "Operator", content: identifier }
+        return { type: "Operator", content: identifier, line: this.line, pos: this.cursor.pos }
       }
 
       default: {
-        return { type: "IdentifierName", content: identifier }
+        return { type: "IdentifierName", content: identifier, line: this.line, pos: this.cursor.pos }
       }
     }
   }
@@ -202,16 +224,17 @@ export default class Lexer {
     const tokens: Token[] = []
     let identifier = ""
     let numericIdentifier = ""
-
+  
     while (!this.cursor.isEOF()) {
       this.nextToken()
-
+  
       while (isSpace(this.cursor.currentTok))
         this.nextToken()
-
-      if (this.cursor.currentTok === '\n')
+  
+      if (this.cursor.currentTok === '\n') {
         this.line++
-
+      }
+  
       // Validation only with alphabetic characters
       // occurs because a variable or another recognizer, token, identifier, etc
       // can not start with a number, because a comparation between if a token is a number or an identifier will overlap
@@ -219,22 +242,22 @@ export default class Lexer {
       // the correct way to declare this variable would be: var test1 = "test"
       if (isAlpha(this.cursor.currentTok)) {
         identifier = ""
-
+  
         // since other recognizers, tokens, identifiers, etc as of the first character
         // can contains numbers, we accept numbers after the first character too
         // example: var t12345 = 10
-
+  
         do {
           identifier += this.cursor.currentTok
         } while (!this.cursor.isEOF() && isAlphaNum(this.nextToken()))
-
+  
         tokens.push(this.resolveIdentifier(identifier))
       }
-
+  
       if (isNumeric(this.cursor.currentTok)) {
         numericIdentifier = ""
         let isFloat = false
-
+  
         do {
           if (this.cursor.currentTok === Tokens.UNDERSCORE) {
             if (this.checkNext(Tokens.UNDERSCORE))
@@ -245,16 +268,22 @@ export default class Lexer {
             isFloat = true
           numericIdentifier += this.cursor.currentTok
         } while (!this.cursor.isEOF() && (isNumeric(this.nextToken()) || this.cursor.currentTok === Tokens.DOT || this.cursor.currentTok === Tokens.UNDERSCORE))
-
+  
         if (!isNumeric(numericIdentifier.charAt(numericIdentifier.length - 1)))
           throw new LexingError(this, "A number must ends with numeric character")
-
-        if (isFloat)
-          tokens.push({ type: "FloatLiteral", content: numericIdentifier })          
+        
+        if (isBinaryLiteral(numericIdentifier))
+          tokens.push({ type: "BinaryLiteral", content: numericIdentifier, line: this.line, pos: this.cursor.pos })
+        else if (isOctalLiteral(numericIdentifier))
+          tokens.push({ type: "OctalLiteral", content: numericIdentifier, line: this.line, pos: this.cursor.pos })
+        else if (isHexaLiteral(numericIdentifier))
+          tokens.push({ type: "HexaLiteral", content: numericIdentifier, line: this.line, pos: this.cursor.pos })
+        else if (isFloat)
+          tokens.push({ type: "FloatLiteral", content: numericIdentifier, line: this.line, pos: this.cursor.pos })
         else
-          tokens.push({ type: "NumericLiteral", content: numericIdentifier })
+          tokens.push({ type: "NumericLiteral", content: numericIdentifier, line: this.line, pos: this.cursor.pos })
       }
-
+  
       /**
        * Finally, if `currentTok` doesnt match with any 'if' statement
        * We match what means every single token, like '+', '-', '*'
@@ -273,27 +302,27 @@ export default class Lexer {
         case Tokens.SEMICOLON:
         case Tokens.EXCLAMATION:
         case Tokens.INTERROGATION: {
-          tokens.push({ type: "Token", content: this.cursor.currentTok })
+          tokens.push({ type: "Token", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         case Tokens.GREATER:
         case Tokens.LESS:
         case Tokens.PLUS: // Minus char is checked below instead after Tokens.PLUS
         case Tokens.STAR: {
-          tokens.push({ type: "Operator", content: this.cursor.currentTok })
+          tokens.push({ type: "Operator", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         
         case Tokens.SLASH: {
-          if (this.checkNext('/')) {
+          if (this.checkNext(Tokens.SLASH)) {
             do {
               this.cursor.currentTok = this.consume() // manual assingment instead use `nextToken` to avoid overlaped comparations
             } while (this.cursor.currentTok !== '\n' && !this.cursor.isEOF())
-          } else if (this.checkNext('*')) {
+          } else if (this.checkNext(Tokens.STAR)) {
             for (;;) {
-              if (this.cursor.currentTok === '*') {
+              if (this.cursor.currentTok === Tokens.STAR) {
                 this.cursor.currentTok = this.consume() // manual assignment instead use `nextToken` to avoid overlaped comparations
-                if (this.cursor.currentTok === '/')
+                if (this.cursor.currentTok === Tokens.SLASH)
                   break
               } else this.nextToken()
             }
@@ -302,73 +331,247 @@ export default class Lexer {
               continue // avoid infinite looping
             else this.nextToken()
           } else {
-            tokens.push({ type: "Operator", content: this.cursor.currentTok })
+            tokens.push({ type: "Operator", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
           }
         } break
-
+  
         /**
          * Minus character (-) is checked below all other operators because it can be followed by a greater character ('>')
          * This means that is a lambda function
          */
         case Tokens.MINUS: {
-          if (this.checkNext(">")) {
+          if (this.checkNext(Tokens.GREATER)) {
             this.nextToken()
-            tokens.push({ type: "Token", content: "->" })
+            tokens.push({ type: "Token", content: "->", line: this.line, pos: this.cursor.pos })
           } else
-            tokens.push({ type: "Operator", content: this.cursor.currentTok })
+            tokens.push({ type: "Operator", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         case Tokens.QUOTE: {
           identifier = this.nextToken()
           
           while (this.nextToken() !== Tokens.QUOTE)
             identifier += this.cursor.currentTok
-
-          tokens.push({ type: "CharLiteral", content: identifier })
+  
+          tokens.push({ type: "CharLiteral", content: identifier, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         case Tokens.BACKSTICK: {
           identifier = this.nextToken()
-
+  
           while (this.nextToken() !== Tokens.BACKSTICK)
             identifier += this.cursor.currentTok
           
-          tokens.push({ type: "StringLiteral", content: identifier })
+          tokens.push({ type: "StringLiteral", content: identifier, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         case Tokens.DQUOTE: {
           identifier = this.nextToken()
-
+  
           while (this.nextToken() !== Tokens.DQUOTE)
             identifier += this.cursor.currentTok
-
+  
           
-          tokens.push({ type: "StringLiteral", content: identifier })
-
+          tokens.push({ type: "StringLiteral", content: identifier, line: this.line, pos: this.cursor.pos })
+  
         } break
-
+  
         case Tokens.COLON: {
           if (this.checkNext(":")) {
             this.nextToken()
-            tokens.push({ type: "Operator", content: "::" })
+            tokens.push({ type: "Operator", content: "::", line: this.line, pos: this.cursor.pos })
           } else
-            tokens.push({ type: "Token", content: this.cursor.currentTok })
+            tokens.push({ type: "Token", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
         } break
-
+  
         case Tokens.DOT: {
           if (this.checkNext('.')) {
             this.nextToken()
             if (this.checkNext('.')) {
               this.nextToken()
-              tokens.push({ type: "Token", content: "..." })
+              tokens.push({ type: "Token", content: "...", line: this.line, pos: this.cursor.pos })
             } else
-              tokens.push({ type: "Token", content: ".."}) // support for slices
+              tokens.push({ type: "Token", content: "..", line: this.line, pos: this.cursor.pos }) // support for slices
           } else
-            tokens.push({ type: "Token", content: this.cursor.currentTok })
+            tokens.push({ type: "Token", content: this.cursor.currentTok, line: this.line, pos: this.cursor.pos })
         } break
       }
     }
-
+  
     return tokens
   }
 }
+
+//! Unused at the moment, `tokens` function may be replaced by a recursive function in future implementations
+// public tokens(): Token {
+  //   let identifier = ""
+  //   let numericIdentifier = ""
+
+  //   while (isSpace(this.cursor.currentTok))
+  //     this.nextToken()
+
+  //   if (this.cursor.currentTok === '\n')
+  //     this.line++
+
+  //   // Validation only with alphabetic characters
+  //   // occurs because a variable or another recognizer, token, identifier, etc
+  //   // can not start with a number, because a comparation between if a token is a number or an identifier will overlap
+  //   // for example, var 1test = "test" is incorrect
+  //   // the correct way to declare this variable would be: var test1 = "test"
+  //   if (isAlpha(this.cursor.currentTok)) {
+  //     identifier = ""
+
+  //     // since other recognizers, tokens, identifiers, etc as of the first character
+  //     // can contains numbers, we accept numbers after the first character too
+  //     // example: var t12345 = 10
+
+  //     do {
+  //       identifier += this.cursor.currentTok
+  //     } while (!this.cursor.isEOF() && isAlphaNum(this.nextToken()))
+
+  //     return this.resolveIdentifier(identifier)
+  //   }
+
+  //   if (isNumeric(this.cursor.currentTok)) {
+  //     numericIdentifier = ""
+  //     let isFloat = false
+
+  //     do {
+  //       if (this.cursor.currentTok === Tokens.UNDERSCORE) {
+  //         if (this.checkNext(Tokens.UNDERSCORE))
+  //           throw new LexingError(this, "Multiple consecutive numeric separators are not permitted")
+  //         this.nextToken()
+  //       }
+  //       if (this.cursor.currentTok === Tokens.DOT)
+  //         isFloat = true
+  //       numericIdentifier += this.cursor.currentTok
+  //     } while (!this.cursor.isEOF() && (isNumeric(this.nextToken()) || this.cursor.currentTok === Tokens.DOT || this.cursor.currentTok === Tokens.UNDERSCORE))
+
+  //     if (!isNumeric(numericIdentifier.charAt(numericIdentifier.length - 1)))
+  //       throw new LexingError(this, "A number must ends with numeric character")
+
+  //     if (isFloat)
+  //       return { type: "FloatLiteral", content: numericIdentifier }
+  //     else
+  //       return { type: "NumericLiteral", content: numericIdentifier }
+  //   }
+
+  //   /**
+  //    * Finally, if `currentTok` doesnt match with any 'if' statement
+  //    * We match what means every single token, like '+', '-', '*'
+  //    */
+  //   switch (this.cursor.currentTok) {
+  //     case Tokens.LBRACE:
+  //     case Tokens.RBRACE:
+  //     case Tokens.LPAREN:
+  //     case Tokens.RPAREN:
+  //     case Tokens.LSQRBR:
+  //     case Tokens.RSQRBR:
+  //     case Tokens.COMMA:
+  //     case Tokens.AMPER:
+  //     case Tokens.EQUAL:
+  //     case Tokens.ESP:
+  //     case Tokens.SEMICOLON:
+  //     case Tokens.EXCLAMATION:
+  //     case Tokens.INTERROGATION: {
+  //       return { type: "Token", content: this.cursor.currentTok }
+  //     }
+
+  //     case Tokens.GREATER:
+  //     case Tokens.LESS:
+  //     case Tokens.PLUS: // Minus char is checked below instead after Tokens.PLUS
+  //     case Tokens.STAR: {
+  //       return { type: "Operator", content: this.cursor.currentTok }
+  //     }
+
+      
+  //     case Tokens.SLASH: {
+  //       if (this.checkNext('/')) {
+  //         do {
+  //           this.cursor.currentTok = this.consume() // manual assingment instead use `nextToken` to avoid overlaped comparations
+  //         } while (this.cursor.currentTok !== '\n' && !this.cursor.isEOF())
+  //       } else if (this.checkNext('*')) {
+
+  //         /* for (;;) {
+  //           if (this.cursor.currentTok === '*') {
+  //             this.cursor.currentTok = this.consume() // manual assignment instead use `nextToken` to avoid overlaped comparations
+  //             if (this.cursor.currentTok === '/')
+  //               break
+  //           } else this.nextToken()
+  //         }
+
+  //         if (this.cursor.isEOF())
+  //           continue // avoid infinite looping
+  //         else this.nextToken() */
+  //       } else {
+  //         return { type: "Operator", content: this.cursor.currentTok }
+  //       }
+  //     } break
+
+  //     /**
+  //      * Minus character (-) is checked below all other operators because it can be followed by a greater character ('>')
+  //      * This means that is a lambda function
+  //      */
+  //     case Tokens.MINUS: {
+  //       if (this.checkNext(">")) {
+  //         this.nextToken()
+  //         return { type: "Token", content: "->" }
+  //       } else
+  //         return { type: "Operator", content: this.cursor.currentTok }
+  //     }
+
+  //     case Tokens.QUOTE: {
+  //       identifier = this.nextToken()
+        
+  //       while (this.nextToken() !== Tokens.QUOTE)
+  //         identifier += this.cursor.currentTok
+
+  //       return { type: "CharLiteral", content: identifier }
+  //     }
+
+  //     case Tokens.BACKSTICK: {
+  //       identifier = this.nextToken()
+
+  //       while (this.nextToken() !== Tokens.BACKSTICK)
+  //         identifier += this.cursor.currentTok
+        
+  //       return { type: "StringLiteral", content: identifier }
+  //     }
+
+  //     case Tokens.DQUOTE: {
+  //       identifier = this.nextToken()
+
+  //       while (this.nextToken() !== Tokens.DQUOTE)
+  //         identifier += this.cursor.currentTok
+
+        
+  //       return { type: "StringLiteral", content: identifier }
+
+  //     }
+
+  //     case Tokens.COLON: {
+  //       if (this.checkNext(":")) {
+  //         this.nextToken()
+  //         return { type: "Operator", content: "::" }
+  //       } else
+  //         return { type: "Token", content: this.cursor.currentTok }
+  //     }
+
+  //     case Tokens.DOT: {
+  //       if (this.checkNext('.')) {
+  //         this.nextToken()
+  //         if (this.checkNext('.')) {
+  //           this.nextToken()
+  //           return { type: "Token", content: "..." }
+  //         } else
+  //           return { type: "Token", content: ".."} // support for slices
+  //       } else
+  //         return { type: "Token", content: this.cursor.currentTok }
+  //     }
+  //   }
+
+  //   if (!this.cursor.isEOF())
+  //     return this.tokens()
+
+  //   return { type: "Unknown", content: this.cursor.currentTok }
+  // }
