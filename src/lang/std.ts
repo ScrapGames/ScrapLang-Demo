9848578470aee2732497700b427c8add0fa6ab90
SuspiciousScrapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-07-13 20:11:46 +0200

    fix: nested functions was causing undefined error

--- src/lang/std.ts
+++ src/lang/std.ts
@@ -1,3 +1,5 @@
+import { existsSync } from "https://deno.land/std@0.224.0/fs/mod.ts"
+
 /**
  * * --- Español --- *
  * std es un modulo global que se incrusta en el scope global del programa justo al empezar el parseo del programa
@@ -12,13 +14,57 @@
  * * --- English --- *
  */
 
-import { ModuleAST } from "../ast/Expressions.ts";
-import { createEmptyScope } from "./scope.ts";
+import * as exp from "../ast/Expressions.ts"
+import { Scope } from "./scope.ts"
+import { RuntimeError } from "./lang-errors.ts";
 
-const STD = new ModuleAST("std", createEmptyScope(null, "std"))
+export function addSTD(scope?: Scope) {
+    if (scope) {
+        scope.addEntry(
+            "log",
+            new exp.PredefinedFunction("<Anonymous>", 1, (args: exp.Expression[]) => {
+                args.forEach(arg => console.log(arg.getValue))
+                return new exp.UndefinedExpression()
+            })
+        )
+        
+        scope.addEntry(
+            "parseInt",
+            new exp.PredefinedFunction("<Anonymous>", 1, (args: exp.Expression[]) => {
+                return new exp.IntegerExpression(parseInt(args[0].getValue as string)) // the received value from `getValue` will be a string
+            })
+        )
 
-STD.insert("console", console)
-STD.insert("parseInt", parseInt)
-STD.insert("parseFloat", parseFloat)
+        scope.addEntry(
+            "readNamesOfDirs",
+            new exp.PredefinedFunction("<Anonymous>", 1, (args: exp.Expression[]) => {
+                const path = args[0].getValue as string
+                if (!existsSync(path))
+                    throw new RuntimeError(`The path '${path}' does not exists`)
 
-export default STD
+                const read = Deno.readDirSync(path)
+                const arrayItems = new exp.ArrayExpression([])
+                const arrayExprVal = (arrayItems.getValue as unknown as Array<string>)
+
+                for (const item of read)
+                    arrayExprVal.push(item.name)
+
+                return arrayItems
+            })
+        )
+
+        scope.addEntry(
+            "arrayAt",
+            new exp.PredefinedFunction("<Anonymous>", 2, (args: exp.Expression[]) => {
+                const array = args[0].getValue as Array<exp.Expression>
+                const position = args[1].getValue as number
+
+                return new exp.Expression(array[position])
+            })
+        )
+        
+        scope.addEntry("true", new exp.Expression(true))
+        scope.addEntry("false", new exp.Expression(false))
+        scope.addEntry("null", null)
+    }
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,11 @@
 import Lexer from "../src/lexer/lexer.ts"
 import Parser from "../src/parser/parser.ts"
-import CompilationError from "../src/lang/compile-error.ts"
+import AST from "../src/ast/ast.ts"
 
+import { CompilationError } from "../src/lang/lang-errors.ts"
 import { inArray } from "../src/utils.ts"
 import { repl } from "../src/repl.ts"
-import AST from "../src/ast/ast.ts"
-import STD from "../src/lang/std.ts"
+import { addSTD } from "../src/lang/std.ts"
 
 const args = Deno.args
 
@@ -22,15 +22,16 @@ async function main() {
             console.warn(`Empty file, nothing to parse in ${lex.cursor.source}`)
         } else {
             const parser = new Parser(lex)
-            parser.addToScope(parser.mainModule.getScope, "std", STD)
+            //! provisional change, this will be replaced in the future by the previous way to embbed STD
+            addSTD(parser.mainModule.getScope)
+
             const ast = AST.from(parser)
-        
             const mainFunction = parser.functions.find(func => func.getName === "main")
             
             if (!mainFunction)
                 throw new CompilationError("Missing program entrypoint (main function)")
             
-            console.log(ast)
+            //console.log(ast)
             parser.warnings.forEach(warning => console.warn("Warning: %s", warning))
         }
     }
