7f8d7fd8095c3e7d98f1dfce07b7880ecc95e71f
SuspiciousScrapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-07-15 09:34:30 +0200

    test: test ready for run programs

--- tests/index.ts
+++ tests/index.ts
@@ -1,12 +1,12 @@
 import Lexer from "../src/lexer/lexer.ts"
 import Parser from "../src/parser/parser.ts"
-import AST from "../src/ast/ast.ts"
 
-import { CompilationError } from "../src/lang/lang-errors.ts"
 import { inArray } from "../src/utils.ts"
 import { repl } from "../src/repl.ts"
 import { addSTD } from "../src/lang/std.ts"
 import { RuntimeError } from "../src/lang/lang-errors.ts"
+import { ScrapCall, ScrapString } from "../src/lang/expressions.ts"
+import { Interpreter } from "../src/interpreter.ts"
 
 const args = Deno.args
 
@@ -14,7 +14,7 @@ async function main() {
     if (inArray("--repl", args)) {
         repl()
     } else {
-    
+
         const fileName = "./tests/tiny.scrap"
         const file = await Deno.readTextFile(fileName)
     
@@ -23,16 +23,18 @@ async function main() {
             console.warn(`Empty file, nothing to parse in ${lex.cursor.source}`)
         } else {
             const parser = new Parser(lex)
-            //! provisional change, this will be replaced in the future by the previous way to embbed STD
             addSTD(parser.mainModule.getScope)
 
-            const ast = AST.from(parser)
+            const interpreter = new Interpreter(parser.build())
+            interpreter.run() // runs the program
+
             const mainFunction = parser.functions.find(func => func.getName === "main")
             
             if (!mainFunction)
                 throw new RuntimeError("Missing program entrypoint (main function)")
+
+            interpreter.execScrapFunction(mainFunction, new ScrapCall(parser.mainModule.getName, mainFunction, Deno.args.map(arg => new ScrapString(arg))))
             
-            //console.log(ast)
             parser.warnings.forEach(warning => console.warn("Warning: %s", warning))
         }
     }
