534cccc01a0ecb8e1f738fad470e911ce8516121
SuspiciousScrapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-07-19 13:41:07 +0200

    feat: added 'Nameable' interface to easily handle objects with 'name' property

--- src/typings.ts
+++ src/typings.ts
@@ -11,6 +11,18 @@ import { ScrapVariable, DefinedFunction, ScrapEntity, ScrapFunction } from "./la
  * }
  */
 export type Nullable<T> = T | null
+/**
+ * Represents values which has `name` as a common property
+ * 
+ * The `name` property is util and fundamental to search and save items in scopes or other data structured
+ * 
+ * These values can include:
+ *  * Any type that extends from `ScrapEntity`, like: variables, classes, modules, etc
+ *  * Any type that extends from `ScrapFunction`
+ */
+export interface Nameable extends ScrapEntity {
+    name: string
+}
 
 /**
  * Represents a function parameter
