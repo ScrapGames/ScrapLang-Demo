a74c3e083fb04140ae630921ba53bbc7c9e463a6
SuspiciousScrapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-07-23 14:15:27 +0200

    refactor: updated method name call

--- src/repl.ts
+++ src/repl.ts
@@ -37,7 +37,7 @@ export function repl() {
             lexer.alsoScan("repl", input)
             parser.restart()
             try {
-                console.log(parser.parsePrimary(parser.mainModule.getScope))
+                console.log(parser.parseRoot(parser.mainModule.getScope))
             } catch (error) {
                 console.error("error:\n %s", error)
             }
