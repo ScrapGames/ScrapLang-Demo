e81b75962e28ba35cb155f236d46a434f8c6ca04
SuspiciousScrapper1 <169595499+SuspiciousScrapper1@users.noreply.github.com>
2024-09-22 09:12:17 +0200

    refactor: ReassignmentNode now handles string (which means handles directly the variable name) isntead of ScrapVariables

--- src/lang/elements/values/reassignment.ts
+++ src/lang/elements/values/reassignment.ts
@@ -1,5 +1,4 @@
 import { ScrapValue } from "@lang/elements/commons.ts"
-import { ScrapVariable } from "@lang/elements/entities/variable.ts"
 
 /**
  * Represents the assignment of a value to a variable. This assignment returns a value
@@ -12,12 +11,13 @@ import { ScrapVariable } from "@lang/elements/entities/variable.ts"
  * // The assignment returns the value that has been assigned. In this last case 20.
  */
 export class ReassignmentExpression extends ScrapValue {
-    private target: ScrapVariable
+    private target: string
 
-    public constructor(target: ScrapVariable, newValue: ScrapValue) {
+    public constructor(target: string, newValue: ScrapValue) {
         super(newValue)
         this.target = target
     }
 
     public get getTarget() { return this.target }
+    public get getValue() { return this.value as ScrapValue }
 }
