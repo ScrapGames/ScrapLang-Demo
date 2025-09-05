cb92b095f65692371b8fa140b3c31fe6d85f5c83
Raxabi <92801687+Raxabi@users.noreply.github.com>
2025-09-06 00:39:44 +0200

    refactor: adjust indentation

--- src/frontend/ast/nodes/index.ts
+++ src/frontend/ast/nodes/index.ts
@@ -1,4 +1,4 @@
 export * as declarations from "@frontend/ast/nodes/declarations.ts"
 export * as statements   from "@frontend/ast/nodes/statements.ts"
 export * as expressions  from "@frontend/ast/nodes/expressions.ts"
-export * as functions from "@frontend/ast/nodes/functions.ts"
+export * as functions    from "@frontend/ast/nodes/functions.ts"
