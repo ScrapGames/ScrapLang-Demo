99ba3b30dd5c8aed79e4d8ff030047140edfefe3
Raxabi <92801687+Raxabi@users.noreply.github.com>
2025-11-16 15:31:06 +0100

    refactor: restructure ASTNode constructor and create index file for AST nodes

--- src/frontend/ast/ast.ts
+++ src/frontend/ast/ast.ts
@@ -1,5 +1,19 @@
 import { Position } from "@frontend/position.ts"
 
 export class ASTNode {
-  public constructor(public start: Position, public end: Position) {}
+  public start: Position
+  public end:   Position
+
+  public constructor(start: Position, end: Position) {
+    this.start = start
+    this.end   = end
+  }
+}
+
+export class AST {
+  public body: ASTNode[]
+
+  public constructor(body: Iterable<ASTNode>) {
+    this.body = Array.from(body)
+  }
 }
